42b3b0ad03ae5abe3298b0cad67cc103f92601b6
T. Mjoen <trond@twined.net>
2021-02-04 09:26:58 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+import '../css/style.postcss'
+
+// import { Dom } from '@univers-agency/jupiter'
+import _ from 'lodash'
+
+console.log(_)
---
+++ vite.config.js
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vite'
+import legacyPlugin from 'vite-plugin-legacy'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    manifest: false,
+    emptyOutDir: true,
+    target: "es2018",
+    outDir: "priv/static", // <- Phoenix expects our files here
+    sourcemap: true, // we want to debug our code in production
+    rollupOptions: {
+      input: {
+        main: "js/index.js"
+      }
+    },
+    terserOptions: {
+      mangle: true,
+      safari10: true,
+      output: {
+        comments: false
+      },
+      compress: {
+        pure_funcs: ['console.info', 'console.debug', 'console.warn'],
+        global_defs: {
+          module: false
+        }
+      }
+    }
+  },
+
+  plugins: [
+    legacyPlugin({
+      targets: [
+        '> 0.5%',
+        'last 2 versions',
+        'Firefox ESR',
+        'not dead',
+      ],
+      polyfills: [
+        'IntersectionObserver',
+      ],
+      ignoreBrowserslistConfig: false,
+      corejs: true
+    })
+  ]
+})
